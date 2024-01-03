6266c13768e1ab3303f18c38a216481339b45367
Taylor Dere <taylordswing@gmail.com>
2024-01-02 21:18:04 -0800

    message

---
+++ backend/mathem/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    rate = models.FloatField()
+
+class UpdateLog(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Test(models.Model):
+    code = models.CharField(max_length=5, unique=True)
+    rate = models.FloatField()
